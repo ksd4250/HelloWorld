7f854d08511903f204b0e90e251e3898807c33b8
mmb2582 <43453083+mmb2582@users.noreply.github.com>
2018-10-04 15:50:42 -0400

    mmb2582

---
+++ app/src/main/java/com/example/kelseydonovan/helloworld/mmb2582__2.java
@@ -0,0 +1,20 @@
+package com.example.kelseydonovan.helloworld;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class mmb2582__2 extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mmb2582__2);
+
+        };
+    }
+
+}
